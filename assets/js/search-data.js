293724550eda87129a4b15060670ad263b416fa9
pyRis <rishu_kumar@live.com>
2025-07-23 09:23:14 +0000

    Deploying to gh-pages from @ pyRis/pyris.github.io@56eb5eb9f40111ea095640f52a6f7d1c4c3d1941 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "For a complete CV, please click the pdf button on the right.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
